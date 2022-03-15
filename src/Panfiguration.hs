32cbc3b44135a3b6543409dd9d28c931b9c44d82
Fumiaki Kinoshita <fumiaki.kinoshita@herp.co.jp>
2022-03-15 15:57:46 +0900

    Move FromParam to a dedicated module

--- src/Panfiguration.hs
+++ src/Panfiguration.hs
@@ -20,65 +20,13 @@ import Barbies.Constraints (Dict(..))
 import Barbies.TH
 import Control.Applicative
 import Data.Bifunctor
-import Data.ByteString.Char8 as BC (ByteString, pack)
-import Data.Char
 import Data.Char
 import Data.Functor.Identity
-import Data.Typeable
 import GHC.Generics ((:*:)(..))
-import Network.Socket (HostName, PortNumber)
 import qualified Options.Applicative as O
 import System.Environment (getEnvironment)
-import Text.Read (readMaybe)
 
--- | A newtype wrapper to distinguish confidential values.
--- 'show' and error messages from 'fromParam' mask its contents.
-newtype Secret a = Secret { unSecret :: a }
-
-instance Show a => Show (Secret a) where
-    show = ('*' <$) . show . unSecret
-
-class FromParam a where
-    -- | Parse a parameter
-    fromParam :: String -> Either String a
-    default fromParam :: (Typeable a, Read a) => String -> Either String a
-    fromParam = readFromParam
-
-    fromParamList :: String -> Either String [a]
-    fromParamList _ = Left "No implementation for fromParamList"
-
--- | A reasonable default implementation for 'fromParam' via 'Read'
-readFromParam :: forall a. (Typeable a, Read a) => String -> Either String a
-readFromParam str = maybe (Left err) Right $ readMaybe str
-    where
-        err = unwords ["failed to parse", str, "as", show (typeRep (Proxy :: Proxy a))]
-
-instance (Typeable a, FromParam a) => FromParam (Secret a) where
-    fromParam str = either (const err) (pure . Secret) $ fromParam str where
-        err = Left $ unwords ["failed to parse", '*' <$ str, "as", show (typeRep (Proxy :: Proxy a))]
-
-instance FromParam Bool where
-    fromParam str = case map toLower str of
-        "false" -> Right False
-        "true" -> Right True
-        _ -> Left "Expected true or false"
-
-instance FromParam Char where
-    fromParam [c] = Right c
-    fromParam _ = Left "Got more than one character"
-    fromParamList = Right
-
-instance FromParam a => FromParam [a] where
-    fromParam = fromParamList
-
-instance FromParam Int
-instance FromParam Integer
-instance FromParam PortNumber
-
-instance FromParam ByteString where
-    fromParam str
-        | all ((<128) . fromEnum) str = Right $ BC.pack str
-        | otherwise = Left "expected ByteString, but found a non-ASCII character"
+import Panfiguration.FromParam
 
 parseEnvs :: (TraversableB h, ConstraintsB h, AllB FromParam h) => h (Const String) -> IO (h Maybe)
 parseEnvs envNames = do
